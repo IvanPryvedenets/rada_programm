9598a05101e4ab3b857935a7cdeda6d66881d9f6
Project <pryvivan@gmail.com>
2020-03-18 17:28:29 +0200

    Second commit - done

--- go_ahead.py
+++ go_ahead.py
@@ -2,11 +2,11 @@ from rada import Rada
 from square import Fraction
 
 
-def main():
+def main():  # Function that take accepts a number in calls methods of the class
     print('Operations with rada')
     print('*' * 20)
 
-    print('1 - Add fraction to the rada\n'
+    print('1 - Add fraction to the rada\n'  # Print all operations available in a Rada class
           '2 - Drop fraction\n'
           '3 - Show fractions\n'
           '4 - Show all deputies\n'
@@ -15,7 +15,7 @@ def main():
     print('Operations with fraction')
     print('*' * 24)
 
-    print('6 - Add deputy to the fraction\n'
+    print('6 - Add deputy to the fraction\n'  # Print all operations available in a Rada class
           '7 - Delete the deputy\n'
           '8 - Show deputies in the fraction\n'
           '9 - Show bribers in the fraction\n'
@@ -24,65 +24,65 @@ def main():
           '12 - Clear the fraction\n'
           '13 - Are deputies in the fraction or not?\n')
 
-    command = int(input('Chose your command: '))
+    command = int(input('Chose your command: '))  # User inputs a command
 
-    rada = Rada()
+    rada = Rada()  # Create a copy of a Rada class
 
-    if command >= 6:
+    if command >= 6:  # If a command variable is rome like 6 than create a Fraction class copy
         fraction = Fraction()
 
-    if command == 1:
+    if command == 1:  # Add a table to the db
         rada.add_fraction()
 
-    elif command == 2:
+    elif command == 2:  # Delete a table in the db
         rada.del_fraction()
 
-    elif command == 3:
+    elif command == 3:  # Show all tables in the db
         rada.show_fractions()
 
-    elif command == 4:
+    elif command == 4:  # Show all persons in the db
         deputies = rada.all_deputies()
 
-        i = 0
+        i = 0  # Create a marker
 
-        while i < len(deputies):
+        while i < len(deputies):  # Do this while a marker isn't equal to length of a list
             print(deputies[i])
-            i += 1
+            i += 1  # + 1 to a marker
 
-    elif command == 5:
+    elif command == 5:  # All persons in the table
         rada.rada_biggest_briber(rada.all_deputies())
 
-    elif command == 6:
+    elif command == 6:  # Add person to the table
         fraction.add_deputy()
 
-    elif command == 7:
+    elif command == 7:  # Delete person in the table
         fraction.del_deputy(None)
 
-    elif command == 8:
+    elif command == 8:  # Show all persons in a table
         deputies = fraction.show_deputies()
         for dep in deputies:
             print(dep)
 
-    elif command == 9:
+    elif command == 9:  # Show all bribers in the table
         bribers = fraction.show_bribers(fraction.show_deputies())
         for brib in bribers:
             print('The briber {} has {} money'.format(brib[1:3], brib[5]))
 
-    elif command == 10:
+    elif command == 10:  # Delete persons in the table that have "brib" marker bigger like 100000
         fraction.del_deputy(fraction.police(fraction.show_bribers(fraction.show_deputies())))
 
-    elif command == 11:
+    elif command == 11:  # Show biggest briber in the table
         fraction.biggest_briber(fraction.show_bribers(fraction.show_deputies()))
 
-    elif command == 12:
+    elif command == 12:  # Clear table
         fraction.clear_fraction()
 
-    elif command == 13:
+    elif command == 13:  # Are table some rows or not
         fraction.are_deputies()
 
-    else:
+    else:  # If command are bigger or smaller like 1 and 13 print next
         print('Try again')
-        main()
+        main()  # Call the a function again
 
 
-main()
+main()  # Call a function
--- rada.py
+++ rada.py
@@ -1,20 +1,18 @@
 import psycopg2.errors
 
-from square import Fraction
-
 
 class Rada:
-    def __init__(self):
+    def __init__(self):  # Create an __init__ method
 
-        self.conn = psycopg2.connect(host="localhost", database="oopadvance", user="ivan", password="password")
+        self.conn = psycopg2.connect(host="localhost", database="oopadvance", user="ivan", password="password")  # Connect to the db
 
-        self.cur = self.conn.cursor()
+        self.cur = self.conn.cursor()  # Create a cursor
 
-        self.sql = """SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'"""
+        self.sql = """SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'"""  # Input the command that will be use quite often
 
-    def add_fraction(self):
+    def add_fraction(self):  # Add table to the db
 
-        fraction = input('Input fraction name: ')
+        fraction = input('Input fraction name: ')  # Input name of a table
 
         self.sql = """CREATE TABLE {} (
                 {}_id SERIAL PRIMARY KEY, 
@@ -25,89 +23,89 @@ class Rada:
                 brib VARCHAR(6) NOT NULL,
                 height VARCHAR(4) NOT NULL,
                 mass VARCHAR(4) NOT NULL
-            )""".format(fraction, fraction)
+            )""".format(fraction, fraction)  # Type the command and fields while will be in the table
 
         try:
-            self.cur.execute(self.sql)
+            self.cur.execute(self.sql)  # Try to create a new table
 
-        except Exception:
+        except Exception:  # If the table is already exist than catch an exception
             print('Similar fraction was already exist\n')
 
-        self.conn.commit()
+        self.conn.commit()  # Save changes
 
-        self.cur.close()
-        self.conn.close()
+        self.cur.close()  # Del the cursor
+        self.conn.close()  # Del the connection
 
-        print('Fraction {} was add successfully\n'.format(fraction))
+        print('Fraction {} was add successfully\n'.format(fraction))  # If all was ok
 
-    def del_fraction(self):
+    def del_fraction(self):  # Delete a table in the db
 
-        fraction = input('Input fraction name: ')
+        fraction = input('Input fraction name: ')  # Input a table name
 
-        self.sql = "DROP TABLE {}".format(fraction)
+        self.sql = "DROP TABLE {}".format(fraction)  # Type the command
 
-        try:
+        try:  # Try do delete a table
             self.cur.execute(self.sql)
             print('Fraction {} was delete successfully\n'.format(fraction))
 
-        except Exception:
+        except Exception:  # If table doesn't not exist print next message
             print('Table {} does not exist\n'.format(fraction))
 
-        self.conn.commit()
+        self.conn.commit()  # Save changes
 
         self.cur.close()
         self.conn.close()
 
-    def show_fractions(self):
+    def show_fractions(self):  # Show all tables in the db
 
-        self.cur.execute(self.sql)
+        self.cur.execute(self.sql)  # Do commands that was created in an __init__ method
 
-        row = self.cur.fetchone()
+        row = self.cur.fetchone()  # Take a row
 
-        while row is not None:
-            print(row[0])
-            row = self.cur.fetchone()
+        while row is not None:  # While row is somethings
+            print(row[0])  # Show a table name
+            row = self.cur.fetchone()  # Go to the next row
 
-    def all_deputies(self):
+    def all_deputies(self):  # Show all persons in a rada
 
-        self.cur.execute(self.sql)
+        self.cur.execute(self.sql)  # Do commands that was created in an __init__ method
 
-        fraction = self.cur.fetchone()
+        fraction = self.cur.fetchone()  # Get a name of fraction
 
-        deputies = list()
+        deputies = list()  # Generate a list that will contain persons
 
-        while fraction is not None:
+        while fraction is not None:  # Check all names of the fractions
 
-            deputies.append('\n' + fraction[0])
+            deputies.append('\n' + fraction[0])  # Append name of a fraction to a list
 
-            self.sql = "SELECT * FROM {}".format(fraction[0])
+            self.sql = "SELECT * FROM {}".format(fraction[0])  # Generate command which help us to get persons in a fraction
 
-            fraction = self.cur.fetchone()
+            fraction = self.cur.fetchone()  # Get a row
 
-            self.cur.execute(self.sql)
+            self.cur.execute(self.sql)  # Insert commands
 
-            deputy = self.cur.fetchone()
+            deputy = self.cur.fetchone()  # Get a first row that has person data
 
-            while deputy is not None:
+            while deputy is not None:  # Check all rows
 
-                deputies.append(deputy)
+                deputies.append(deputy)  # Add person data to the list
 
-                deputy = self.cur.fetchone()
+                deputy = self.cur.fetchone()  # Go to a next row
 
-        return deputies
+        return deputies  # Return a list
 
-    def rada_biggest_briber(self, bribers):
+    def rada_biggest_briber(self, bribers):  # Show a biggest briber in a rada
 
-        for brib in bribers:
-            if type(brib) == str:
-                bribers.remove(brib)
+        for brib in bribers:  # For person in list
+            if type(brib) == str:  # If "fraction name" - delete this thing because a list contain tuple - person data and str - name of fractions
+                bribers.remove(brib)  # Delete a fraction name
 
-        money = [int(brib[5]) for brib in bribers]
+        money = [int(brib[5]) for brib in bribers]  # Get value of "brib" marker
 
-        money.sort(reverse=True)
+        money.sort(reverse=True)  # Sort integers from biggest to smallest
 
-        brib = [brib for brib in bribers if int(brib[5]) == money[0]]
+        brib = [brib for brib in bribers if int(brib[5]) == money[0]]  # Find person that has a biggest "brib" marker
 
-        print(brib[0])
+        print(brib[0])  # Show this person
 
 
